018c8a2160cdb20449753f8291620a99f4af1b48
Terrys101 <97230300+Terrys101@users.noreply.github.com>
2022-04-28 13:15:31 +0100

    add map

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+const map = L.map('map').setView([0,0], 0);
+const marker = L.marker([0,0]).addTo(map);
+const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
+
+
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 async function getISS(){
 const response = await fetch(api_url);
 const data = await response.json();
 const {latitude, longitude} = data
+marker.setLatLng([latitude,longitude])
 document.getElementById('latitude').textContent = latitude;
 document.getElementById('longitude').textContent = longitude
 
 
 }
 getISS();
+
+
+
+
+
+
